1cf9c948fb83813a61bae20b2f75cc87d4185ce2
Tess Watt <71218139+Tess314@users.noreply.github.com>
2020-10-31 17:53:56 +0000

    Update Pi.cs

--- Pi.cs
+++ Pi.cs
@@ -1,4 +1,8 @@
-﻿using System;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
 
 namespace Pi
 {
@@ -19,6 +23,35 @@ namespace Pi
         {
             Console.WriteLine("Pi: " + pi);
             Console.WriteLine("Digits (after decimal point): " + (digits.Length));
+        }
+
+        public void SaveImage()
+        {
+            string text = temp.Trim();
+
+            Bitmap bmp = new Bitmap(1, 1);
+
+            Font font = new Font("Free 3 of 9", 25, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            Graphics graphics = Graphics.FromImage(bmp);
+
+            int width = (int)graphics.MeasureString(text, font).Width;
+            int height = (int)graphics.MeasureString(text, font).Height;
+
+            bmp = new Bitmap(bmp, new Size(width, height));
+            graphics = Graphics.FromImage(bmp);
+
+            graphics.Clear(Color.Cyan);
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+
+            graphics.DrawString(text, font, new SolidBrush(Color.Black), 0, 0);
+
+            graphics.Flush();
+            graphics.Dispose();
+
+            //if you want to save the image  uncomment the below line.
+            bmp.Save(@"C:\pi.jpg", ImageFormat.Jpeg);
         }
     }
 }
