475f65129dcc54ee5ba1c5e0bdc341623cedfa23
Tess Watt <71218139+Tess314@users.noreply.github.com>
2020-10-31 14:15:15 +0000

    Add files via upload

---
+++ Pi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pi
+{
+    class Pi
+    {
+        private double pi;
+        private string temp;
+        private string digits;
+
+        public void PiCalc()
+        {
+            pi = Math.PI;
+            temp = pi.ToString();
+            digits = temp.Substring(2);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Pi: " + pi);
+            Console.WriteLine("Digits (after decimal point): " + (digits.Length));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pi p = new Pi();
+            p.PiCalc();
+            p.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
